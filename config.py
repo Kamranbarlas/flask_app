74a2fd720b85b3236c748e5144e9fda14d554ccf
MKB365 <Kamran_Barlas@hotmail.com>
2021-07-19 19:19:42 +0500

    DEPLOYMENT

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
 
-
 class Config:
     SECRET_KEY = 'e6b1bb2211fe6a1f17af62811f296f7b'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587       #465
+    MAIL_PORT = 587  # 465
     MAIL_USE_TLS = True
 
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
